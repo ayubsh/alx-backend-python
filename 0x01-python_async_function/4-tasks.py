94533bf16235b7494dc78c80083bcdaf79e0a897
ayubsh <ibrahimayubsheikh@gmail.com>
2023-10-09 11:01:28 -0700

    tasks runner

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""coroutine with async"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """execute multip coroutine
+    """
+    waits = await asyncio.gather(*tuple(map(
+        lambda _: task_wait_random(max_delay), range(n))))
+    return sorted(waits)
